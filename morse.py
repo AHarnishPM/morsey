1cb7be42909c8f3923f9ae46bb456b7c6b763fdd
AHarnish <95876538+AHarnishPM@users.noreply.github.com>
2023-09-15 16:03:44 -0400

    Update to Python2, cleaned, added comments
    -Updated old Python 2 code to run well in Python 3.9
    -Cleaned up nasty elif chains, replaced with dictionary system
    -Added comments

--- morse.py
+++ morse.py
@@ -1,357 +1,93 @@
-from textwrap import wrap
 import time
 from playsound import playsound
 
+# Dictionary containing all accepted morse symbols and their english counterparts.
+morseToEnglishDict = {"_": " ", ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f", "--.": "g",
+                      "....": "h",
+                      "..": "i", ".---": "j", "-.-": "k", ".-..": "l", "--": "m", "-.": "n", "---": "o", ".--.": "p",
+                      "--.-": "q",
+                      ".-.": "r", "...": "s", "-": "t", "..-": "u", "...-": "v", ".--": "w", "-..-": "x", "-.--": "y",
+                      "--..": "z",
+                      ".----": "1", "..---": "2", "...--": "3", "....-": "4", ".....": "5", "-....": "6", "--...": "7",
+                      "---..": "8", "----.": "9", "-----": "0"}
 
-
+# Calls functions of morse.py after user input
 def init():
-    direction = raw_input("> ")
+    direction = input("> ")
     if direction == "1":
-        etom()
+        englishToMorse()
     elif direction == "2":
-        mtoe()
+        morseToEnglish()
     elif direction == "q":
         exit()
     else:
-        print "Type 1, 2, or q"
+        print("Type 1, 2, or q")
         init()
 
+#Prints the english equivalent of inputted morse code
+#Input ".../---/..." prints "sos"
+def morseToEnglish():
+    print("Type morse in this format:")
+    print(".../---/.../_/.../---/... = sos sos")
 
-def dot():
-    print "DOT"
-    playsound("beep10.mp3")
+    morseInput = input("> ")
+    inputSliced = morseInput.split("/")
 
-def dash():
-    print "DASH"
-    playsound("beep30.mp3")
+    print(inputSliced)
 
+    output = ""
+    for i in inputSliced:
+        output += morseToEnglishDict[i]
 
-def mtoe():
-    print "Type morse in this format:"
-    print ".../---/.../_/.../---/.../ = sos sos"
-    print "You must put a / after every letter or space"
-
-    inp = raw_input("> ")
-    inpslc = wrap(inp, 1)
-
-    letter = ""
-    wrd = ""
-
-    for i in inpslc:
-        if i == "/":
-            if letter == "_":
-                wrd += " "
-
-            elif letter == ".-":
-                wrd += "a"
-
-            elif letter == "-...":
-                wrd += "b"
-
-            elif letter == "-.-.":
-                wrd += "c"
-
-            elif letter == "-..":
-                wrd += "d"
-
-            elif letter == ".":
-                wrd += "e"
-
-            elif letter == "..-.":
-                wrd += "f"
-
-            elif letter == "--.":
-                 wrd += "g"
-
-            elif letter == "....":
-                 wrd += "h"
-
-            elif letter == "..":
-                wrd += "i"
-
-            elif letter == ".---":
-                 wrd += "j"
-
-            elif letter == "-.-":
-                 wrd += "k"
-
-            elif letter == ".-..":
-                 wrd += "l"
-
-            elif letter == "--":
-                 wrd += "m"
-
-            elif letter == "-.":
-                 wrd += "n"
-
-            elif letter == "---":
-                 wrd += "o"
-
-            elif letter == ".--.":
-                 wrd += "p"
-
-            elif letter == "--.-":
-                 wrd += "q"
-
-            elif letter == ".-.":
-                 wrd += "r"
-
-            elif letter == "...":
-                 wrd += "s"
-
-            elif letter == "-":
-                 wrd += "t"
-
-            elif letter == "..-":
-                 wrd += "u"
-
-            elif letter == "...-":
-                 wrd += "v"
-
-            elif letter == ".--":
-                 wrd += "w"
-
-            elif letter == "-..-":
-                 wrd += "x"
-
-            elif letter == "-.--":
-                 wrd += "y"
-
-            elif letter == "--..":
-                 wrd += "z"
-
-            elif letter == ".----":
-                wrd += "1"
-
-            elif letter == "..---":
-                wrd += "2"
-
-            elif letter == "...--":
-                wrd += "3"
-
-            elif letter == "....-":
-                wrd += "4"
-
-            elif letter == ".....":
-                wrd += "5"
-
-            elif letter == "-....":
-                wrd += "6"
-
-            elif letter == "--...":
-                wrd += "7"
-
-            elif letter == "---..":
-                wrd += "8"
-
-            elif letter == "----.":
-                wrd += "9"
-
-            elif letter == "-----":
-                wrd += "0"
-
-            else:
-                print "Error:Unknown value"
-
-            letter = ""
-
-        else:
-            letter += i
-
-    print wrd
+    print(output)
 
     time.sleep(2)
     start()
 
-
-
-
-def etom():
-    print "Use underscores instead of spaces"
-    eng = raw_input("input> ")
-    enletrs = wrap(eng, 1)
-
-    binline = ""
-
-    for i in enletrs:
-        if i == "a":
-            binline += "102"
-
-
-        elif i == "b":
-            binline += "2010101"
-
-
-        elif i == "c":
-            binline += "2010201"
-
-
-        elif i == "d":
-            binline += "20101"
-
-
-        elif i == "e":
-            binline += "1"
-
-
-        elif i == "f":
-            binline += "1010201"
-
-
-        elif i == "g":
-            binline += "20201"
-
-
-        elif i == "h":
-            binline += "1010101"
-
-
-        elif i == "i":
-            binline += "101"
-
-
-        elif i == "j":
-            binline += "1020202"
-
-
-        elif i == "k":
-            binline += "20102"
-
-
-        elif i == "l":
-            binline += "1020101"
-
-
-        elif i == "m":
-            binline += "202"
-
-
-        elif i == "n":
-            binline += "201"
-
-
-        elif i == "o":
-            binline += "20202"
-
-
-        elif i == "p":
-            binline += "1020201"
-
-
-        elif i == "q":
-            binline += "2020102"
-
-
-        elif i == "r":
-            binline += "10201"
-
-
-        elif i == "s":
-            binline += "10101"
-
-
-        elif i == "t":
-            binline += "2"
-
-
-        elif i == "u":
-            binline += "10102"
-
-
-        elif i == "v":
-            binline += "1010102"
-
-
-        elif i == "w":
-            binline += "10202"
-
-
-        elif i == "x":
-            binline += "2010102"
-
-
-        elif i == "y":
-            binline += "2010202"
-
-
-        elif i == "z":
-            binline += "2020101"
-
-
-        elif i == "_":
-            binline += "0"
-
-
-        elif i == "1":
-            binline += "102020202"
-
-
-        elif i == "2":
-            binline += "101020202"
-
-
-        elif i == "3":
-            binline += "101010202"
-
-
-        elif i == "4":
-            binline += "101010102"
-
-
-        elif i == "5":
-            binline += "101010101"
-
-
-        elif i == "6":
-            binline += "201010101"
-
-
-        elif i == "7":
-            binline += "202010101"
-
-
-        elif i == "8":
-            binline += "202020101"
-
-
-        elif i == "9":
-            binline += "202020201"
-
-
-        elif i == "0":
-            binline += "202020202"
-
-
-        else:
-            print "error: unrecognized input"
-
-        binline += "000"
-
-    for i in binline:
-        if i == "2":
-            dash()
-        elif i == "1":
-            dot()
-        elif i == "0":
+# Prints the english input as morse code sound plays, then prints the morse code in same format as before
+def englishToMorse():
+    #Swaps keys and values of englishToMorseDict, could be more efficient to just include the dict.
+    englishToMorseDict = {}
+    for k, v in morseToEnglishDict.items():
+        englishToMorseDict[v] = k
+
+    englishInput = input("English input > ")
+
+    morseReturn = ""
+
+    for i in englishInput:
+        morse = englishToMorseDict[i]
+        print(i, end="")
+        playMorseSound(morse)
+        morseReturn += "/" + morse
+
+    print(morseReturn)
+
+    time.sleep(2)
+    start()
+
+#Helper function for englishToMorse(), plays the morse sound.
+def playMorseSound(morse):
+    for i in morse:
+        # Plays short dit morse sound
+        if i == ".":
+            pass
+            # playsound("beep10.mp3") lost old sound files
+        elif i == ",":
+            pass
+            # playsound("beep30.mp3") lost old sound files
+        elif i == "/":
+            #Sleeps same length as a dit
             time.sleep(0.1)
-        else:
-            print "Error: not 2 or 1 or 0"
 
-    print binline
-
-    time.sleep(2)
-    start()
-
-
-
+#Called when morse.py is run and 2s after etom or mtoe functions are completed.
 def start():
-    print "Morse Code Translator"
-    print "coded by Aaron Harnish"
-    print "----------------------"
-    print "Type \'1\' for english to morse"
-    print "Type \'2\' for morse to english"
-    print "Type \'q\' to exit program"
+    print("----------------------")
+    print("Type \'1\' for english to morse")
+    print("Type \'2\' for morse to english")
+    print("Type \'q\' to exit program")
 
     init()
+
 
 start()
