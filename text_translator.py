938b90861d3144e69e394163ae14b969ea9f1313
AHarnish <95876538+AHarnishPM@users.noreply.github.com>
2023-10-16 17:01:39 -0400

    Update text_translator.py
    Solved a few inconsitencies

--- text_translator.py
+++ text_translator.py
@@ -8,7 +8,7 @@ def morse_text_to_english_text(code: str) -> str:
     Translates morse code text to English text.
 
     Parameter code: morse code str to be translated. Must be of type str and formatted like "--- ...
-    --- / --- ... ---". Letters separated by " ", words separated by " / "
+    --- / --- ... ---". Letters separated by " ", words separated by ' / '
 
 
     For debugging purposes within outside implementations, unsupported morse code chars will not throw an error and
@@ -27,7 +27,7 @@ def morse_text_to_english_text(code: str) -> str:
         '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!',
         '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':',
         '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
-        '.-..-.': '"', '...-..-': '$', '.--.-': '@',
+        '.-..-.': '"', '...-..-': '$', '.--.-.': '@',
         # You can add more Morse code characters as needed.
     }
 
@@ -36,6 +36,7 @@ def morse_text_to_english_text(code: str) -> str:
 
     # Split input code into list of 'char' strings
     code_split = code.split(' ')
+
     english_text = ""
 
     # Translate each 'char' and append to output string.
@@ -56,6 +57,7 @@ def english_text_to_morse_text(english_text: str) -> str:
     Translates english text to morse code text.
 
     Parameter english_text: English text str to be translated.
+    Ex: "HI BOB" will return ".... ../-... --- -.../
 
     For debugging purposes within outside implementations, unsupported english letters will not throw an error and
     will instead add a '#' to the output string and print an error message before continuing execution.
@@ -81,6 +83,9 @@ def english_text_to_morse_text(english_text: str) -> str:
         raise TypeError("Input must be of type str")
 
     english_text = english_text.upper()
+
+    # Split this up so that there are no spaces before and after '/'
+
     code = ""
     err_count = 1
 
@@ -93,4 +98,5 @@ def english_text_to_morse_text(english_text: str) -> str:
             english_text += "# "
             err_count += 1
 
+    code = code.rstrip(' ')
     return code
